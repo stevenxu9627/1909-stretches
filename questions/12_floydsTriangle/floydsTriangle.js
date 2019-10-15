49a99ef33bf10975c4b0e08458db35dc7cef513c
hypedis <m.bae@live.com>
2019-10-15 13:02:16 -0400

    floyds triangle

---
+++ questions/12_floydsTriangle/floydsTriangle.js
@@ -0,0 +1,11 @@
+// Given a number of rows n, print out Floyd's Triangle
+// Example:
+//  input: n = 3
+//  output:
+//  1
+//  23
+//  456
+
+const floydsTriangle = n => {};
+
+module.exports = { floydsTriangle };
