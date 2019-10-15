74477c1fd165456d81876560840a2b0bc5777b86
hypedis <m.bae@live.com>
2019-10-15 15:17:49 -0400

    thank u next

---
+++ questions/18_thankUnext/thankUnext.js
@@ -0,0 +1,19 @@
+// create an array method called "next" that console.logs
+// the next index in the array when called.
+
+/*
+ Example:
+ someArr = ['shake', 'and', 'bake']
+ someArr.next() // "shake"
+ someArr.next() // "and" 
+ someArr.next() // "bake"
+*/
+
+// hint: you are adding functionality directly to the
+// javascripts global Array object
+
+// ****no test writing required on this stretch
+
+// YOUR CODE (intentionally blank):
+
+// no export statement required
