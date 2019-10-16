8104cd5fed161c09674d8e21021a051d790fd64f
stevenxu9627 <stevenxu9627@gmail.com>
2019-10-16 19:07:33 -0400

    10/16-1

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,11 +1,37 @@
-function groupBy(arr, attr) {
-let arr=[];
-for(let i=0;i<arr.length;i++){
-    arr.forEach(number => {
-        let result=arr[i].length;
-        return `${result:['','']}`;
-    });
-}
-}
+// function groupBy(arr, attr) {
+//     // let arr=[];
+//     // for(let i=0;i<arr.length;i++){
+//     //     arr.forEach(number => {
+//     //         let result=arr[i].length;
+//     //         return `${result:['','']}`;
+//     //     });
+//     // }
+
+//     // let arr =[];
+//     // arr.array.forEach(element => {
+//     //     for (let i=0;i<arr.length;i++){
+//     //         let number=arr[i].length;
+//     //         return `${result:['', '']}`;
+//     //     }
+//     // });
+
+
+// }
+
+const groupBy=(collection,attr)=>collection.reduce((groupDict, nextItem)=>{
+    let groupDictKey;
+    
+    if (typeof attr === 'function'){
+        groupDictKey= attr(nextItem);
+    }else{
+        groupDictKey=nextItem[attr];
+    }
+
+    groupDict[groupDictKey]= !groupDict[groupDictKey] 
+    ? [nextItem]
+    : groupDict[groupDictKey].concat([nextItem]);
+
+    return groupDict;
+},{});
 
 module.exports = { groupBy };
