8432be207daf1864355556d3004c3cd3723c3882
Russell Moore <russellwmoore@gmail.com>
2019-11-13 18:39:58 -0500

    Cache (#14)

--- questions/00_isEqual/isEqual.js
+++ questions/00_isEqual/isEqual.js
@@ -3,6 +3,16 @@ function isEqual(obj1, obj2) {
   // run your tests by running `jest isEqual`
   // you can also run tests on every save by running
   // `jest --watch isEqual`
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (const key in obj1) {
+    if (!obj2[key] || obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = { isEqual };
