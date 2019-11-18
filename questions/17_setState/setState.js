2a42464edcd54192de99f0c59b2aa88a7028d218
Mark Bae <markbae@Marks-MacBook-Pro.local>
2019-11-17 23:22:31 -0500

    setState

---
+++ questions/17_setState/setState.js
@@ -0,0 +1,14 @@
+// create a method called setState that updates the current state of the class
+
+// DONT need to write your own tests for this stretch
+
+class StatefulThing {
+  constructor(initialState = {}) {
+    this.state = initialState;
+  }
+  setState() {
+    // YOUR CODE
+  }
+}
+
+module.exports = { StatefulThing };
