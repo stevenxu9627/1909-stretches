0c4f776741f243ced532d5b9e1656fd6d1bccc51
stevenxu9627 <stevenxu9627@gmail.com>
2019-11-25 18:42:40 -0500

    clean up

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -1,10 +1,15 @@
-function cache(a) {
-    if (typeof a !== 'function') {
-        throw 'Input must be a function.';
-    } else {
-        return a;
-    }
-
+function cache(cb) {
+    if (typeof cb !== 'function') { throw 'Input must be a function.' };
+    const memo = [];
+    return function (...num) {
+        if (num in memo) {
+            return memo[num];
+        } else {
+            memo[num] = cb(...num);
+            return memo[num];
+        };
+    };
 }
+
 
 module.exports = { cache };
--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -3,19 +3,35 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder(data, order, names) {
-    const newData = [];
-    const data = [{ order, names }];
-    if (typeof order === 'number') {
-        for (let i = 0; i <= order.value; i++) {
-            data.map((name, i) => {
-                newData.push(data[i].names)
-            })
-            return newData;
-        }
-    } else if (typeof order === 'string') {
+// function sortOrder(data, order, names) {
+//     // const newData = [];
+//     // const data = [{ order, names }];
+//     // if (typeof order === 'number') {
+//     //     for (let i = 0; i <= order.value; i++) {
+//     //         data.map((name, i) => {
+//     //             newData.push(data[i].names)
+//     //         })
+//     //         return newData;
+//     //     }
+//     // } else if (typeof order === 'string') {
 
-    }
+//     // }
+
+// }
+
+function sortOrder(arr, sorted, key) {
+    return arr.sort((a, b) => {
+        if (a[sorted] < b[sorted]) {
+            return -1;
+        }
+        if (a[sorted] > b[sorted]) {
+            return 1;
+        }
+        return 0;
+    }).map(element => {
+        return element[key]
+    })
+
 }
 
 module.exports = { sortOrder };
